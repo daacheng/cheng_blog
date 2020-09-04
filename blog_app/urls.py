f93008fa9ac54aae97b62e0a8caa27fe0d37296f
luzhicheng <luzhicheng@whbaihongsoft.com>
2020-09-04 16:03:34 +0800

    函数视图改为类视图 Signed-off-by: luzhicheng <luzhicheng@whbaihongsoft.com>

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls import url, include
 
 app_name = 'blog_app'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:pk>', views.detail, name='detail'),
-    path('archives/<int:year>/<int:month>', views.archive, name='archive'),
-    path('categorys/<int:pk>', views.category, name='category'),
-    path('tags/<int:pk>', views.tag, name='tag'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts/<int:pk>', views.PostDetailView.as_view(), name='detail'),
+    path('archives/<int:year>/<int:month>', views.ArchiveView.as_view(), name='archive'),
+    path('categorys/<int:pk>', views.CategoryView.as_view(), name='category'),
+    path('tags/<int:pk>', views.TagView.as_view(), name='tag'),
     url(r'mdeditor/', include('mdeditor.urls')),
 ]
 
--- blog_app/views.py
+++ blog_app/views.py
@@ -5,6 +5,72 @@ import markdown
 import re
 from markdown.extensions.toc import TocExtension
 from django.utils.text import slugify
+from django.views.generic import ListView, DetailView
+
+
+class IndexView(ListView):
+    # 类视图
+    model = Post  # 指定模型
+    template_name = 'blog_app/index.html'  # 指定要渲染的页面
+    context_object_name = 'post_list'  # 指定获取的模型列表数据保存的变量名，这个变量会被传递给模板。
+
+
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog_app/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        # 在类视图中，从URL捕获的路径参数值保存在实例的kwargs属性里(字典)，非路径参数值保存在实例的args属性里(列表)。
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(category=cate)
+
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog_app/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(tag=tag)
+
+
+class ArchiveView(ListView):
+    model = Post
+    template_name = 'blog_app/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super().get_queryset().filter(create_time__year=year, create_time__month=month)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog_app/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 重写父类方法，每次访问时候执行，记录阅读量
+        response = super().get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        # 重写父类方法，对返回的body进行处理
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+        # 自动生成文章目录 如果没有目录，则不显示
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+        return post
 
 
 def index(request):
